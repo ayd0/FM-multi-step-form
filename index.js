0a838b088a7a6c4f6cd3338af48ef0aa3e90db6d
ayd0 <73216273+ayd0@users.noreply.github.com>
2023-09-26 14:14:07 -0700

    added plan card, began work on addon card

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const navBtns = [...document.querySelectorAll(".nav-btn")];
 let selectedNav = 0;
+const formCards = document.querySelectorAll('.form-card');
 
 const setSelectedNav = (e) => {
     navBtns[selectedNav].classList.remove('selected-nav');
+    formCards[selectedNav].style.display = 'none';
     selectedNav = parseInt(e.target.innerText) - 1;
     navBtns[selectedNav].classList.add('selected-nav');
+    formCards[selectedNav].style.display = 'block';
 }
 
 navBtns.forEach(navBtn => navBtn.addEventListener('click', (e) => setSelectedNav(e)));
