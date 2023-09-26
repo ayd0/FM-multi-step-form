0bf6d4a83f7d70be42efc740f22a70d509774b19
ayd0 <73216273+ayd0@users.noreply.github.com>
2023-09-26 15:53:54 -0700

    added logic for updating navs, added triggers for footer nav buttons

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const navBtns = [...document.querySelectorAll(".nav-btn")];
 let selectedNav = 0;
-const formCards = document.querySelectorAll('.form-card');
+const formCards = document.querySelectorAll(".form-card");
+const nextStepBtn = document.querySelector("#footer div button");
+const prevStepBtn = document.querySelector("#footer div p");
 
-const setSelectedNav = (e) => {
-    navBtns[selectedNav].classList.remove('selected-nav');
-    formCards[selectedNav].style.display = 'none';
-    selectedNav = parseInt(e.target.innerText) - 1;
+const updateSelectedNav = (target) => {
+    if (
+        (target < selectedNav && target >= 0) ||
+        (target > selectedNav && target < formCards.length)
+    ) {
+        navBtns[selectedNav].classList.remove("selected-nav");
+        formCards[selectedNav].style.display = "none";
+        selectedNav = target;
 
-    navBtns[selectedNav].classList.add('selected-nav');
-    formCards[selectedNav].style.display = 'block';
-}
+        navBtns[selectedNav].classList.add("selected-nav");
+        formCards[selectedNav].style.display = "block";
+    }
+};
 
-navBtns.forEach(navBtn => navBtn.addEventListener('click', (e) => setSelectedNav(e)));
+navBtns.forEach((navBtn) =>
+    navBtn.addEventListener("click", (e) =>
+        updateSelectedNav(parseInt(e.target.innerText) - 1)
+    )
+);
+nextStepBtn.addEventListener("click", () => updateSelectedNav(selectedNav + 1));
+prevStepBtn.addEventListener("click", () => updateSelectedNav(selectedNav - 1));
